0c5659836f0c57b3222e93754f7d4480ed38bf34
jautung <jautung.chan@yale.edu>
2021-03-07 11:46:49 -0500

    Base of MCTS

--- src/players/MCTS.cpp
+++ src/players/MCTS.cpp
@@ -2,12 +2,63 @@
 #include "../game/GraphicsHandler.hpp"
 #include "Players.hpp"
 #include <random>
+#include <tuple>
+#include <unordered_map>
+#include <vector>
 
 MCTSPlayer::MCTSPlayer(GameStateHandler* initGameStateHandler, GraphicsHandler* initGraphicsHandler, int initIters) : Player(initGameStateHandler, initGraphicsHandler) {
   iters = initIters;
 }
 
+MCTSPlayer::~MCTSPlayer() {}
+
 move_t MCTSPlayer::selectMove(state_t state, colormode_t colorMode) {
+  for (int i = 0; i < iters; i++) {
+    runIter(state);
+  }
+  return selectBestMove(state);
+}
+
+void MCTSPlayer::runIter(state_t state) {
+
+}
+
+move_t MCTSPlayer::selectBestMove(state_t state) {
   auto moves = gameStateHandler->allMoves(state);
-  return *std::next(std::begin(moves), rand() % moves.size());
+  std::vector<move_t> bestMoves;
+  std::tuple<int, int> bestChildInfo = std::make_tuple(0, 1); // initialized to infinite win rate, since want to select child with lowest win rate
+  for (auto move : moves) {
+    auto childState = gameStateHandler->swapPlayers(gameStateHandler->makeMove(state, move));
+    auto childCachedInfo = cache.find(childState);
+    std::tuple<int, int> childInfo;
+    if (childCachedInfo == cache.end()) { // child has not been searched before, assume baseline 50% win rate
+      childInfo = std::make_tuple(2, 1);
+    } else {
+      childInfo = childCachedInfo->second;
+    }
+    if (worseInfo(childInfo, bestChildInfo)) {
+      bestMoves.clear();
+      bestMoves.push_back(move);
+      bestChildInfo = childInfo;
+    } else if (equalInfo(childInfo, bestChildInfo)) {
+      bestMoves.push_back(move);
+    }
+  }
+  return *std::next(std::begin(bestMoves), rand() % bestMoves.size());
+}
+
+bool MCTSPlayer::equalInfo(std::tuple<int, int> infoA, std::tuple<int, int> infoB) {
+  auto numVisitsA = std::get<0>(infoA);
+  auto numWinsA = std::get<1>(infoA);
+  auto numVisitsB = std::get<0>(infoB);
+  auto numWinsB = std::get<1>(infoB);
+  return numWinsA * numVisitsB == numWinsB * numVisitsA; // numWinsA / numVisitsA == numWinsB / numVisitsB
+}
+
+bool MCTSPlayer::worseInfo(std::tuple<int, int> infoA, std::tuple<int, int> infoB) {
+  auto numVisitsA = std::get<0>(infoA);
+  auto numWinsA = std::get<1>(infoA);
+  auto numVisitsB = std::get<0>(infoB);
+  auto numWinsB = std::get<1>(infoB);
+  return numWinsA * numVisitsB < numWinsB * numVisitsA; // numWinsA / numVisitsA < numWinsB / numVisitsB
 }
--- src/players/Players.hpp
+++ src/players/Players.hpp
@@ -3,6 +3,8 @@
 #include "../game/GraphicsHandler.hpp"
 #include "../optimal/OptComputer.hpp"
 #include "../utils/DataHandler.hpp"
+#include <tuple>
+#include <unordered_map>
 
 class Player {
   public:
@@ -51,7 +53,13 @@ class HeuristicSimplePlayer : public Player {
 class MCTSPlayer : public Player {
   public:
     MCTSPlayer(GameStateHandler* initGameStateHandler, GraphicsHandler* initGraphicsHandler = NULL, int initIters = 0);
+    ~MCTSPlayer();
     move_t selectMove(state_t state, colormode_t colorMode) override;
   private:
     int iters;
+    std::unordered_map<state_t, std::tuple<int, int>> cache; // map from state to (2 * number of visits, 2 * number of X wins + 1 * number of draws)
+    void runIter(state_t state);
+    move_t selectBestMove(state_t state);
+    bool equalInfo(std::tuple<int, int> infoA, std::tuple<int, int> infoB);
+    bool worseInfo(std::tuple<int, int> infoA, std::tuple<int, int> infoB);
 };
